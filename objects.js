eb88868514b527e39436bb7ff8b03801a6687aa0
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-20 01:24:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: songTitle})
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlistartistName
+  delete playlist[artistName]
+  return playlist
 }

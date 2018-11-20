3a53561d1408e5a32196266b17caade1881b2f06
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-20 01:06:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {Wu_Tang: "Slow Blues"}
 function updatePlaylist(playlist, artistName, songTitle) {
+  var playlist = playlist
   return Object.assign(playlist, {artistName: songTitle})
 }
